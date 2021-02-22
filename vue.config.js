4129a15425ee0ef903413b0949120ac5d62c3135
wanghao <529036324@qq.com>
2021-02-22 13:52:40 +0800

    优化

--- vue.config.js
+++ vue.config.js
@@ -14,7 +14,7 @@ module.exports = {
   publicPath: IS_PROD ? process.env.VUE_APP_PUBLIC_PATH : './', // 默认'/'，部署应用包时的基本 URL
   // outputDir: process.env.outputDir || 'dist', // 'dist', 生产环境构建文件的目录
   // assetsDir: "static", // 相对于outputDir的静态资源(js、css、img、fonts)目录
-  lintOnSave: false,
+  lintOnSave: !IS_PROD,
   runtimeCompiler: true, // 是否使用包含运行时编译器的 Vue 构建版本
   productionSourceMap: !IS_PROD, // 生产环境的 source map
   parallel: require('os').cpus().length > 1,
@@ -144,8 +144,8 @@ module.exports = {
   },
   devServer: {
     // overlay: { // 让浏览器 overlay 同时显示警告和错误
-    //   warnings: true,
-    //   errors: true
+    //     warnings: true,
+    //     errors: true
     // },
     // open: false, // 是否打开浏览器
     // host: "localhost",
